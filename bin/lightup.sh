07ccae357ce54b53875248d6cc959e97c1eea445
Shannon Lee <slee@Susan-Constant.local>
2011-08-19 22:08:55 -0700

    wooooo!  fuck yeah!

---
+++ bin/lightup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+echo "deb     http://deb.torproject.org/torproject.org maverick main" > /etc/apt/sources.list.d/tor.list
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+apt-get update
+apt-get install -y --force-yes tor tor-geoipdb git
+cd /opt
+git clone git://github.com/noisetor/ops
+
+CPUS=$(cat /proc/cpuinfo  | grep "^processor	" | wc -l);
+
+for i in $(seq 1 $CPUS); do
+	cp /opt/ops/configs/torrc /etc/tor/torrc.${i}
+	RPORT=$((442 + $i));
+	DIRPORT=$((79 + $i))
+	DATADIR="/var/lib/tor${i}"
+	PIDFILE="/var/run/tor/tor${i}.pid"
+	mkdir -p $DATADIR;
+	sed -i "s/RPort 443/RPort $RPORT/g" /etc/tor/torrc.${i}
+	sed -i "s/DirPort 80/RPort $DIRPORT/g" /etc/tor/torrc.${i}
+	sed -i "s/DataDirectory \/var\/lib\/tor/DataDirectory $DATADIR/g" /etc/tor/torrc.${i}
+	sed -i "s/PidFile \/var\/lib\/tor/PidFile $PIDFILE/g" /etc/tor/torrc.${i}
+done
---
+++ bin/run.sh
@@ -0,0 +1,1 @@
+aws run ami-cef405a7 -k slee -t t1.micro --user-data-file lightup.sh
